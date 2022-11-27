ee2799735556d41134a0e37469895dcd76fc6652
Ivan Chuvilin <i_chuvilin@outlook.com>
2022-11-27 16:36:46 +0700

    add dispatcher

--- dispatcher/src/main/java/com/ichuvilin/DispatcherApplication.java
+++ dispatcher/src/main/java/com/ichuvilin/DispatcherApplication.java
@@ -1,7 +1,11 @@
 package com.ichuvilin;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatcherApplication {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		SpringApplication.run(DispatcherApplication.class);
 	}
 }
--- dispatcher/src/main/java/com/ichuvilin/controller/TelegramBot.java
+++ dispatcher/src/main/java/com/ichuvilin/controller/TelegramBot.java
@@ -1,2 +1,45 @@
-package com.ichuvilin.controller;public class TelegramBot {
+package com.ichuvilin.controller;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+@Log4j
+public class TelegramBot extends TelegramLongPollingBot {
+
+	@Value("${bot.name}")
+	private String botName;
+	@Value("${bot.token}")
+	private String botToken;
+
+	@Override
+	public String getBotUsername() {
+		return botName;
+	}
+
+	@Override
+	public String getBotToken() {
+		return botToken;
+	}
+
+	@Override
+	public void onUpdateReceived(Update update) {
+		if (update.hasMessage()) {
+			log.debug("New Message: " + update.getMessage().getText());
+			var chatId = update.getMessage().getChatId();
+			var sendMessage = new SendMessage();
+			sendMessage.setText("Dispatcher");
+			sendMessage.setChatId(chatId);
+			try {
+				execute(sendMessage);
+			} catch (TelegramApiException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
 }
